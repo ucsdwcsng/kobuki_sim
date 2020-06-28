0eb856b7fdf9f104ac2e1e9f385f2d5c5c5627a2
jbisarra3 <jbbisarr@ucsd.edu>
2020-06-27 23:40:50 -0700

    modified:   ../../bagfiles/bagFileRead.py 	modified:   ../../bagfiles/cartographer_parse.sh 	modified:   ../../bagfiles/cartographer_record.sh 	modified:   ../../bagfiles/recordGraph.py 	modified:   mapping.launch 	../../bagfiles/bagfile_cartographer_run.sh 	../../bagfiles/get_bagfile.sh 	../../bagfiles/trajectory.py 	../../frames.gv 	../../frames.pdf 	../config_files/kobuki_yd_combined.lua 	../config_files/kobuki_yd_imu_only.lua 	../config_files/kobuki_yd_lidar_only.lua 	cartographer_bag_record.launch 	record_bag_files.launch 	../worlds/box_world

--- bagfiles/cartographer_parse.sh
+++ bagfiles/cartographer_parse.sh
@@ -1,8 +1,6 @@
-rosservice call /finish_trajectory 0
-rosservice call /write_state "{filename: '${HOME}/bagfiles/record_files/output.bag.pbstream'}"
-roslaunch cartographer_ros assets_writer_backpack_2d.launch bag_filenames := ${HOME}/bagfiles/record_files/output.bag pose_graph_filename := ${HOME}/bagfiles/record_files/output.bag.pbstream
+#roslaunch cartographer_ros assets_writer_backpack_2d.launch bag_filenames := ${HOME}/bagfiles/record_files/$1.bag pose_graph_filename := ${HOME}/bagfiles/record_files/$1.bag.pbstream
 
 
-python ~/kobuki_sim/bagfiles/bagFileRead.py -i output.bag
-python ~/kobuki_sim/bagfiles/tf_parse_trajectory.py tf_link.txt
+#python ~/kobuki_sim/bagfiles/bagFileRead.py -i output.bag
+#python ~/kobuki_sim/bagfiles/tf_parse_trajectory.py tf_link.txt
 python ~/kobuki_sim/bagfiles/tf_parse_trajectory.py tf_footprint.txt
--- bagfiles/recordGraph.py
+++ bagfiles/recordGraph.py
@@ -30,20 +30,20 @@ plt.axis('equal')
 plt.plot(odomx[len(odomx)-1], odomy[len(odomy)-1], marker = 'v')
 plt.title('Position based on \odom')
 
-odom_combinedx = []
-odom_combinedy = []
-with open('odom_combinedData.csv') as f:
-	coordinates = csv.reader(f)
-	for row in coordinates:
-		odom_combinedx.append(float(row[0]))
-		odom_combinedy.append(float(row[1]))
+#odom_combinedx = []
+#odom_combinedy = []
+#with open('odom_combinedData.csv') as f:
+#	coordinates = csv.reader(f)
+#	for row in coordinates:
+#		odom_combinedx.append(float(row[0]))
+#		odom_combinedy.append(float(row[1]))
 
-plt.subplot(2,3,2)
-plt.plot(odom_combinedx, odom_combinedy, marker='o')
-plt.plot(odom_combinedx[0],odom_combinedy[0], marker = '^')
-plt.axis('equal') 
-plt.plot(odom_combinedx[len(odom_combinedx)-1], odom_combinedy[len(odom_combinedy)-1], marker = 'v')
-plt.title('Position based on \\robot_pose_ekf\odom_combined')
+#plt.subplot(2,3,2)
+#plt.plot(odom_combinedx, odom_combinedy, marker='o')
+#plt.plot(odom_combinedx[0],odom_combinedy[0], marker = '^')
+#plt.axis('equal') 
+#plt.plot(odom_combinedx[len(odom_combinedx)-1], odom_combinedy[len(odom_combinedy)-1], marker = 'v')
+#plt.title('Position based on \\robot_pose_ekf\odom_combined')
 
 
 modelx = []
@@ -55,42 +55,72 @@ with open('modelStates.csv') as f:
 		modely.append(float(row[1]))
 
 
-plt.subplot(2,3,3)
+plt.subplot(2,3,2)
 plt.plot(modelx, modely, marker='o')
 plt.plot(modelx[0],modely[0], marker = '^')
-plt.axis('equal')
+#plt.axis('equal')
 plt.plot(modelx[len(modelx)-1], modely[len(modely)-1], marker = 'v') 
 plt.title('Position based on \gazebo\model_states')
-	 
+
 #check position calculations for imu data
-#time = []
-#accelx = [0]
-#accely = [0]
-#velx = [0]
-#vely = [0]
-#posx = [0]
-#posy = [0]
-#with open('imuData.csv') as f:
-#	acceleration = csv.reader(f)
-#	for row in acceleration:
-#		time.append(float(row[0]))
-#		accelx.append(float(row[1]))
-#		accely.append(float(row[2]))
+time = []
+accelx = [0]
+accely = [0]
+velx = [0]
+vely = [0]
+posx = [0]
+posy = [0]
+with open('imuData.csv') as f:
+	acceleration = csv.reader(f)
+	for row in acceleration:
+		time.append(float(row[0]))
+		accelx.append(float(row[1]))
+		accely.append(float(row[2]))
 
-#time.insert(0, (2*time[0]-time[1])) 
-#
-#for i in range (1, len(time)-1):
-#	velx.append(velx[i-1] + accelx[i] * (time[i]-time[i-1]))
-#	posx.append(posx[i-1] + velx[i] * (time[i]-time[i-1]))
-#	vely.append(vely[i-1] + accely[i] * (time[i]-time[i-1]))
-# 	posy.append(posy[i-1] + vely[i] * (time[i]-time[i-1]))
+time.insert(0, (2*time[0]-time[1])) 
 
-#plt.subplot(2,3,4)
-#plt.plot(posx, posy, marker='o')
-#plt.axis('equal')
-#plt.plot(posx[0],posy[0], marker = '^')
-#plt.plot(posx[len(posx)-1], posy[len(posy)-1], marker = 'v') 
-#plt.title('Position based on \mobile_base\sensors\imu_data')
+for i in range (1, len(time)-1):
+	velx.append(velx[i-1] + accelx[i] * (time[i]-time[i-1]))
+	posx.append(posx[i-1] + velx[i] * (time[i]-time[i-1]))
+	vely.append(vely[i-1] + accely[i] * (time[i]-time[i-1]))
+	posy.append(posy[i-1] + vely[i] * (time[i]-time[i-1]))
+
+plt.subplot(2,3,3)
+plt.plot(posx, posy, marker='o')
+plt.axis('equal')
+plt.plot(posx[0],posy[0], marker = '^')
+plt.plot(posx[len(posx)-1], posy[len(posy)-1], marker = 'v') 
+plt.title('Position based on \imu')
+	 
+#check position calculations for mobile base imu data
+time = []
+accelx = [0]
+accely = [0]
+velx = [0]
+vely = [0]
+posx = [0]
+posy = [0]
+with open('mobileBaseData.csv') as f:
+	acceleration = csv.reader(f)
+	for row in acceleration:
+		time.append(float(row[0]))
+		accelx.append(float(row[1]))
+		accely.append(float(row[2]))
+
+time.insert(0, (2*time[0]-time[1])) 
+
+for i in range (1, len(time)-1):
+	velx.append(velx[i-1] + accelx[i] * (time[i]-time[i-1]))
+	posx.append(posx[i-1] + velx[i] * (time[i]-time[i-1]))
+	vely.append(vely[i-1] + accely[i] * (time[i]-time[i-1]))
+	posy.append(posy[i-1] + vely[i] * (time[i]-time[i-1]))
+
+plt.subplot(2,3,4)
+plt.plot(posx, posy, marker='o')
+plt.axis('equal')
+plt.plot(posx[0],posy[0], marker = '^')
+plt.plot(posx[len(posx)-1], posy[len(posy)-1], marker = 'v') 
+plt.title('Position based on \mobile_base\sensors\imu_data')
 
 
 ##check how to collect tf data
